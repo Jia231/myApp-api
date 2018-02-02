fe78292a75dbefeb109c3d8b88a1e2525d05e9f7
Jia Ming Liou <dliou471@gmail.com>
2018-02-01 22:37:49 -0600

    Movie to dashboard

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -5,15 +5,11 @@ import mongoose from 'mongoose';
 const router = express.Router();
 
 router.post("/", (req, res) => {
-    //
     const data = req.body.movie;
-    console.log(data)
     Movie.findOne({ userId: data.userId, movieId: data.movieId })
         .then(movie => {
-            console.log(movie)
             if (movie == null) {
                 const newMovie = new Movie({ ...req.body.movie })
-                console.log(newMovie)
                 return newMovie.save()
                     .then(() => res.json({ message: "Movie stored successfully" }))
                     .catch(err => res.status(400).json({ errors: { global: err } }))
@@ -24,12 +20,18 @@ router.post("/", (req, res) => {
         })
 })
 
-export default router;
+router.post('/userCollection', (req, res) => {
+    const data = req.body.userId;
+    Movie.find({ userId: data }).then(
+        movies => {
+            if (movies != null) {
+                return res.json({ movies: movies })
+            }
+            else {
+                return res.json({})
+            }
+        }
+    ).catch(err => res.status(400).json({ errors: { global: err } }))
+})
 
-/*
-res.json({ message: movie })
-            if(!!movie)res.status(400)
-            .json({ errors: { global: "Movie is already in your collection" } }))
-    movie.save()
-        .then(() => res.json({ message: "Movie stored successfully" }))
-        .catch(err => res.status(400).json({ errors: { global: err } }))*/
+export default router;
